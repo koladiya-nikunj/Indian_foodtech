857023b87b8bf31467b478db3300e832fd04d068
koladiya-nikunj <koladiyanikunj124@gmail.com>
2024-12-11 10:11:17 +0530

    remove Indian_foodtech from route

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/Indian_foodtech/',
+    // base: '/Indian_foodtech/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
